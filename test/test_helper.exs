cbe69866fe0a46790078d598ba967434fee6d987
Joe-noh <goflb.jh@gmail.com>
2014-10-05 15:52:09 +0900

    add assert_encoding helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,9 @@
 ExUnit.start()
+
+defmodule TestHelper do
+  defmacro assert_encoding(filename, expected) do
+    quote do
+      assert FileEncoding.judge("test/test_files/" <> unquote(filename)) == unquote(expected)
+    end
+  end
+end
